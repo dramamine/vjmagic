2c8eff1f5d3bee1df466f0dec6b5157fee15fbcc
marten silbiger <marten@metal-heart.org>
2017-02-25 17:04:26 -0800

    got this class workin'

--- push/graphics.py
+++ push/graphics.py
@@ -1,15 +1,24 @@
+import itertools;
 
 class Graphics:
-	def coords_to_quadrant(coordA, coordB):
-		# 0,0 is midi 36
-		# 8,8 is midi 105
-		(xa, ya) = coordA
-		(xb, yb) = coordB
-		origin = 36 + (ya * 8) + xa
-		return list(itertools.chain(
-		  range(36, 40),
-		  range(44, 48),
-		  range(52, 56),
-		  range(60, 64),
-		  range(101, 105)
-		))
+  def __init__(self):
+    return
+
+  # (0,0), (3,3) gives you the lower left quadrant
+  # (0,0), (7,7) gives you the whole thing
+  # 0,0 is midi 36
+  # 7,7 is midi 99
+  def coords_to_quadrant(self, coord_a, coord_b):
+    (xa, ya) = coord_a
+    (xb, yb) = coord_b
+
+    width = xb - xa + 1
+
+    ranges = list()
+    for y_iterator in range(ya, yb + 1):
+      point = 36 + (y_iterator * 8) + xa
+      ranges.append(range(
+        point, point + width
+      ))
+
+    return list(itertools.chain.from_iterable(ranges))
--- test/graphicsSpec.py
+++ test/graphicsSpec.py
@@ -1,13 +1,32 @@
 # run from midi directory
-# python -m unittest discover project_directory "*Spec.py"
-
+# python testrunner.py
+#
 import unittest
-from push.graphics import graphics
+from push.graphics import Graphics
 class TestGraphics(unittest.TestCase):
-	def test_coords_to_quadrant(self):
-		g = new Graphics()
-		res = g.coords_to_quadrant((0,0), (8,8))
-		self.assertTrue(res)
+
+  def test_coords_to_quadrant(self):
+    g = Graphics()
+    res = g.coords_to_quadrant((0,0), (3,3))
+    self.assertTrue(36 in res)
+    self.assertTrue(39 in res)
+    self.assertTrue(40 not in res)
+
+    self.assertTrue(60 in res)
+    self.assertTrue(63 in res)
+    self.assertTrue(64 not in res)
+
+    res = g.coords_to_quadrant((0,0), (7,7))
+    print res
+    # lower left
+    self.assertTrue(36 in res)
+    self.assertTrue(43 in res)
+    self.assertTrue(44 in res)
+
+    # upper right
+    self.assertTrue(99 in res)
+    self.assertTrue(100 not in res)
+
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
